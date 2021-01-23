4754355ae10e8eec51d7a384ccf3fe707c9448c3
Jason Russell <jasrusable@gmail.com>
2021-01-23 18:59:21 +0200

    Add shebang

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable @typescript-eslint/no-floating-promises */
